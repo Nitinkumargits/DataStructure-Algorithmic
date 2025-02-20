566ccb2b5dc863f6082fbd131b3cf2e6accefd20
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2025-02-20 21:52:04 +0530

    bitwise , IOStream, Pointer, Vector

---
+++ C++/vector.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    vector<char> v = {'a', 'c', 'f', 'd', 'z'};
+
+    // Accessing and printing values using indexes
+    cout << v[3] << endl;
+    cout << v.at(2);
+
+    return 0;
+}
