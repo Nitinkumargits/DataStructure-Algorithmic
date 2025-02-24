705fa2b8c891c0caa52cbcb1a13a7cfd1fbd30b2
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2025-02-24 14:09:52 +0530

    talyor series

---
+++ C++/Rect.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+class Rect
+{
+private:
+    /* data */
+    int length;
+    int breadth;
+
+public:
+    Rect(/* args */);
+    ~Rect();
+};
+
+Rect::~Rect()
+{
+}
+
+Rect::Rect(/* args */)
+{
+}
+
+Rect::~Rect()
+{
+}
+
+int main()
+{
+}
