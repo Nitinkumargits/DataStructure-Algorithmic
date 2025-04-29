c52afeb3c5cb5fbd3e346b1ede139bd76e7fc01f
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2025-04-29 22:11:32 +0530

    code for boolena " caplocks"

--- JavaScript/fetch.js
+++ JavaScript/fetch.js
@@ -13,4 +13,27 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+let arrayInterger3 = [1, 2, 3, 4, 5];
+let arrayInterger2 = [1, 2, 3, 4, 5];
+// showRecipe();
+// /////////////////////////////
+
+let arrayInterger = [1, 2, 3, 4, 5];
+
+console.log("Original arry : Before :", arrayInterger); //Original arry : Before : [ 1, 2, 3, 4, 5 ]
+
+let arrayIntergerS = arrayInterger.splice(3, 1, "a", "b", "c");
+
+console.log("Spliced ", arrayIntergerS); ////Spliced  [ 4 ]
+
+console.log("Original arry : After :", arrayInterger); //Original arry : After : [ 1,   2,   3, 'a','b', 'c', 5 ]
+
+// let arrayIntergerS2 = arrayInterger1.splice(3);
+// let arrayIntergerS3 = arrayInterger1.splice(3, 1, "a", "b", "c");
+
+// console.log(arrayIntergerS2);
+// console.log(arrayIntergerS3);
+
+////////////////////////////////////////////////////////////////////
+
+<script></script>;
