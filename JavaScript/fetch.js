d4ed327292b69d8f4c4eddf33ee638219a183832
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2025-04-29 10:35:04 +0530

    fetching API

--- JavaScript/fetch.js
+++ JavaScript/fetch.js
@@ -1,12 +1,16 @@
+const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+
 const showRecipe = async function () {
   try {
-    const res = await fetch(
-      `https://forkiy-api.com.herokuapp.com/api/v2/recipes/534ed66`
-    );
+    const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes");
+
+    if (!res.ok) throw new Error(`${res.status} ${res.statusText} 💔`);
 
     const data = await res.json();
-    console.log(data);
+    console.log("data :", data);
   } catch (error) {
-    console.log(error);
+    console.log("ERROR : ", error);
   }
 };
+
+showRecipe();
